56ec89ec3a73cf915752fd9147e6af9f6208d5fe
Maria Treier <mariatraier@gmail.com>
2024-04-11 18:57:18 +0200

    feat: complete part of the task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -111,8 +111,14 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises)
+    .then((responses) => {
+      return responses;
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
 }
 
 /**
@@ -127,8 +133,15 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return result.value;
+      }
+      return null;
+    })
+  );
 }
 
 /**
